3fb5aa2cdb4112225df69368dfaae0d01bd0d9ee
Your Name <gim.kobayashi@gmail.com>
2018-09-24 23:34:59 +0900

    update

---
+++ cold_start_modeling/30-get_target.py
@@ -0,0 +1,19 @@
+import shelve
+
+db = shelve.open('../cold_start_dataset2/spam')
+
+targets = []
+for key in list(db.keys()):
+  obj = db[key]
+  try:
+    num = int(obj['clip'])
+  except Exception as ex:
+    print(ex)
+    num = 100
+  targets.append(num)
+
+import pandas as pd
+
+df = pd.DataFrame({'target':targets})
+
+df.to_csv('target.csv', index=None)
