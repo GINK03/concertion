a617ada76a23b23db048f43ef71487750d178cdb
nard <gim.kobayashi@gmail.com>
2018-10-02 13:55:27 +0900

    update

--- backends/vippers.jp/find_mongo.py
+++ backends/vippers.jp/find_mongo.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 
-db = client['news_dwango']
+db = client['vipper_jp']
 collection = db['collection']
 
 for post in collection.find():
