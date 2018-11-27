29b3446b2c88106c13bb5bc8feb65bec6c334da4
namagon <namagon@users.noreply.github.com>
2018-11-27 17:54:03 +0900

    delete

--- app/ai_news/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AiNewsConfig(AppConfig):
-    name = 'ai_news'
--- app/app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-# webpackが生成したhtmlをそのままテンプレートとして読み込ませる
-def index(request):
-    return render(request, 'index.html', {})
