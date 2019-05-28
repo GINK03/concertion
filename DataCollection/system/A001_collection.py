4470938aceaccb5f2d07a06ac5370941182ac905
gimpei <gim.kobayashi@gmail.com>
2019-05-29 00:13:06 +0900

    updae

---
+++ DataCollection/system/A001_collection.py
@@ -0,0 +1,63 @@
+import requests
+import bs4
+import json
+import pickle
+from sqlitedict import *
+import datetime
+from concurrent.futures import ProcessPoolExecutor as PPE
+import itertools
+import pandas as pd
+import re
+'''
+TABLE DEFINITIONS
+{
+    'LAST_UPDATE': datetime,
+    'HTMLS': [{'DATE':datetime, 'HTML':html }],
+    'DELETED':boolean,
+}
+'''
+def pmap(arg):
+    ii = arg['arg']
+    db = SqliteDict('db.sqlite', encode=pickle.dumps, decode=pickle.loads, autocommit=True)
+    for i in ii:
+        url = f'https://togetter.com/li/{i}'
+        # DELTEDされていたら取得しない
+        if db.get(url) is not None and db.get(url)['DELETED'] == True:
+            continue
+        # 5回以上スクレイピングされていたら取得しない
+        if db.get(url) is not None and len(db.get(url)['HTMLS']) >= 5:
+            continue
+        r = requests.get(url)
+        soup = bs4.BeautifulSoup(r.text)
+        if soup.find('div', {'class':'alert alert-info'}) is not None:
+            db[url] = {'DELETED':True}
+            continue
+        if db.get(url) is None:
+            db[url] = {'LAST_UPDATE': datetime.datetime.now(), 'HTMLS':[], 'DELETED':False}
+        print(soup.title.text, url)
+        obj = db[url]
+        obj['HTMLS'].append({'DATE':datetime.datetime.now(), 'HTML': r.text})
+        # update
+        db[url] = obj
+        db.commit()
+
+
+def run():
+    '''
+    最も最近に投稿されたまとめから最大値を逆算する
+    '''
+    r = requests.get('https://togetter.com/recent')
+    thumbs = bs4.BeautifulSoup(r.text, 'lxml').find('div', {'class':'topics_box'}).find_all('a', {'class':'thumb'})
+    thumb = thumbs[0]
+    max_url = thumb.get('href')
+    max_post_id = re.search(r'https://togetter.com/li/(.*?$)', max_url).group(1)
+    if max_post_id.isdigit() is False:
+        print('There is any wrong to get latest id.')
+        exit(1)
+
+    args = [[i] for i in reversed(range(1200000, int(max_post_id)))]
+    keys = [i[0]%16 for i in args]
+    df = pd.DataFrame({'arg':args, 'key':keys}).groupby(by=['key']).sum().reset_index()
+    args = df.to_dict('records')
+    with PPE(max_workers=16) as exe:
+        exe.map(pmap, args)
---
+++ DataCollection/system/B001_sql_to_csv.py
@@ -0,0 +1,67 @@
+import random
+from sqlitedict import *
+import pickle
+import bs4
+import pandas as pd
+import json
+from concurrent.futures import ProcessPoolExecutor as PPE
+
+
+
+def pmap(arg):
+    key, urls = arg
+    db = SqliteDict('db.sqlite', encode=pickle.dumps,
+                    decode=pickle.loads)
+    objs = []
+    random.shuffle(urls)
+    db_b = SqliteDict('db_b.sqlite', encode=pickle.dumps,
+                 decode=pickle.loads, autocommit=True)
+    for idx, url in enumerate(urls):
+        val = db[url]
+        if val.get('DELETED') is None:
+            continue
+        if val['DELETED'] is True:
+            continue
+        if db_b.get(url) is not None:
+            continue
+        HTMLS = val['HTMLS']
+        soup = bs4.BeautifulSoup(HTMLS[-1]['HTML'])
+        parse_date = HTMLS[-1]['DATE']
+        try:
+            tags = [a.text for a in soup.find('span', {'class':'tag_box'}).find_all('a', {'class':'rad_btn tag_type_1'})] 
+            pub_date = soup.find('span', {'class': 'info_date'}).get('content')
+            icon_view = soup.find('span', {'class': 'icon_view'}).text
+            title = soup.title.text
+            tweets = ' '.join(
+                [t.text for t in soup.find_all('div', {'class': 'tweet'})])
+            obj = {'PUB_DATE': pub_date, 'TITLE': title,
+                    'TWEET': tweets, 'ICON_VIEW': icon_view, 'URL': url, 'TAGS':json.dumps(tags, ensure_ascii=False)}
+            db_b[url] = obj
+            print(idx, len(urls), url)
+            
+        except Exception as ex:
+            print(ex, url)
+    del db_b
+
+def run():
+    db_ = SqliteDict('db.sqlite', encode=pickle.dumps,
+                     decode=pickle.loads, autocommit=True)
+    args = {}
+    for idx, url in enumerate(db_.keys()):
+        #print(idx, url)
+        key = idx % 16
+        if args.get(key) is None:
+            args[key] = []
+        args[key].append(url)
+    args = [(key, urls) for key, urls in args.items()]
+    del db_
+    #[pmap(arg) for arg in args]
+    objs = []
+    with PPE(max_workers=16) as exe:
+        exe.map(pmap, args)
+    db = SqliteDict('db_b.sqlite', encode=pickle.dumps,
+             decode=pickle.loads)
+    pd.DataFrame(list(db.values())).to_csv('local.csv', index=None)
+
+if __name__ == '__main__':
+    run()
