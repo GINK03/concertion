84151c87a1ac7aabaa450f0a5bfd51cd276f2abf
gimpei kobayashi <gim.kobayashi@gmail.com>
2019-06-25 00:34:22 +0900

    remove gomi

--- DataCollection/system/temp.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('local.csv')
-
-df['PARSED_DATE'] = df['PUB_DATE'].apply(lambda x: pd.to_datetime(x, format='%Y-%m-%d', errors='ignore'))
-df = df[pd.notnull(df['PARSED_DATE'])]
-df['PARSED_DATE'].apply(lambda x:print(x.strftime('%Y %V')))
