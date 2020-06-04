12584db2b71f61da614932e319f60278af7bfcdd
gimpei.kobayashi <gim.kobayashi@gmail.com>
2020-06-04 14:57:56 +0900

    fix absolute path problems

--- ContentsProvider/run_gunicorn.sh
+++ ContentsProvider/run_gunicorn.sh
@@ -1,1 +1,1 @@
-gunicorn -w 16 --bind 0.0.0.0:443 --chdir /home/gimpei/ikazuchi/nvme1n1/concertion/ContentsProvider/project wsgi --keyfile /etc/letsencrypt/live/concertion.page/privkey.pem --certfile /etc/letsencrypt/live/concertion.page/fullchain.pem
+gunicorn -w 16 --bind 0.0.0.0:443 --chdir $PWD/project wsgi --keyfile /etc/letsencrypt/live/concertion.page/privkey.pem --certfile /etc/letsencrypt/live/concertion.page/fullchain.pem
